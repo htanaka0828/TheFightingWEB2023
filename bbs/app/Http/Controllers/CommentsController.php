b776d88a5c73e0415592c9da57413b996c41b9d2
たなかひかる <h.tanaka@holyday.co.jp>
2023-11-15 20:20:08 +0900

    commentを取ってくるところの修正

--- bbs/app/Http/Controllers/CommentsController.php
+++ bbs/app/Http/Controllers/CommentsController.php
@@ -10,7 +10,10 @@ class CommentsController extends Controller
 {
     public function index()
     {
-        $coomentAll = Comment::all();
+        $coomentAll = Comment::query()
+            ->join('accounts', 'account_id', '=', 'accounts.id')
+            ->select('name', 'comment', 'comments.created_at as create_date')
+            ->get();
         return view('comments')->with('comments', $coomentAll);
     }
 }
